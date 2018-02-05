4fa069a91f07669c343e9d97faeaf2c2478ab548
notanumber11 <denisromasanta@gmail.com>
2018-02-05 18:05:44 +0100

    Finish blog sample app

---
+++ src/components/posts_index.js
@@ -0,0 +1,46 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchPosts } from "../actions";
+
+class PostsIndex extends Component {
+  componentDidMount() {
+    console.log("Mounting and fetching posts...")
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          <Link to={`/post/${post.id}`}>
+            {post.title}
+          </Link>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/post/new">
+            Add a Post
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, PropsRoute, Switch } from "react-router-dom";
+import promise from "redux-promise"
 
-import { BrowserRouter, Route, PropsRoute } from "react-router-dom";
 import reducers from './reducers';
-import PostIndex from "./components/post_index"
+import PostsIndex from "./components/posts_index"
+import PostNew from "./components/post_new"
+import PostsShow from "./components/posts_show"
 
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component = { PostIndex } something="foo"/>
+        <Switch>
+          <Route path="/post/new" component = { PostNew }/>
+          <Route path="/post/:id" component = { PostsShow }/>
+          <Route path="/" component = { PostsIndex }/>
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
